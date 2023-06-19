4c806768a89a9840e794f8711b8792f4f2f9328c
Gordon Chau <gordonchau47@gmail.com>
2023-06-19 20:04:51 +1000

    Filled out a bit of bill and order classes

--- wms/Bill.py
+++ wms/Bill.py
@@ -1,4 +1,20 @@
 class Bill:
+    # Constructor for the final bill
     def __init__(self, price):
         self.__price = price
         self.__paid = False
+
+    # Getters for price and paid status
+    def get_price(self):
+        return self.__price
+    
+    def is_paid(self):
+        return self.__paid
+    
+    # Method to accumulate bill from menu items
+    def add_price(self, price):
+        self.__price += price
+
+    # When bill is paid, set bool to true
+    def pay(self):
+        self.__paid = True
--- wms/Order.py
+++ wms/Order.py
@@ -1,4 +1,7 @@
 import itertools
+import Bill
+import Deal
+import MenuItem
 
 class Order:
 
@@ -9,6 +12,7 @@ class Order:
         self.__id = next(Order.__id_iter)
         self.__menu_items = []
         self.__deals = []
+        self.__bill = Bill(0)
         #self.__status: State (NOT YET IMPLEMENTED)
     
     def __init__(self, menu_items):
@@ -20,3 +24,31 @@ class Order:
         self.__id = next(Order.__id_iter)
         self.__menu_items = menu_items
         self.__deals = deals
+
+    # Getters
+    def get_bill(self):
+        return self.__bill
+    
+    def get_deals(self):
+        return self.__deals
+    
+    def get_menu_items(self):
+        return self.__menu_items
+    
+    # Adding and removing deal items to the order
+    def add_deal(self, deal):
+        self.__deals.append(deal)
+        # self.get_bill().add_price(deal.get_discount())
+
+    def remove_deal(self, deal):
+        self.__deals.remove(deal)
+    
+    # Adding and removing menu items to the order
+    def add_menu_item(self, menuItem):
+        self.__menu_items.append(menuItem)
+        # self.get_bill().add_price(menuItem.get_price())
+
+    def remove_menu_item(self, menuItem):
+        self.__menu_items.remove(menuItem)
+    
+    # def change_state():
