6df4e95def17b0a9d3e301af75ae0820ef189291
Gordon Chau <gordonchau47@gmail.com>
2023-06-19 18:57:14 +1000

    Filling out user class with constructor for non-logged in users and adding getters

--- wms/User.py
+++ wms/User.py
@@ -4,8 +4,19 @@ class User:
     # Unique identifier starting from 0
     __id_iter = itertools.count()
 
+    # Constructor for non-logged in user
+    def __init__(self):
+        self.__id = next(User.__id_iter)
+
+    # Constructor for logged in user
     def __init__(self, firstname, lastname):
         self.__id = next(User.__id_iter)
         self.__firstname = firstname
         self.__lastname = lastname
-        
+
+    # Getters
+    def get_firstname():
+        return self.__firstname
+
+    def get_lastname():
+        return self.__lastname
