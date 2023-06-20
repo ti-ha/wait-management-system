47c60b7d48e2ecf238f2dddeb87f74125256e903
TheiSam <samialhilfi@gmail.com>
2023-06-20 22:17:48 +1000

    Created simple customer page with Back to Landing Page button, and placeholders for Menu Categories and Menu Items with mock data

--- frontend/src/components/Customer/Customer.js
+++ frontend/src/components/Customer/Customer.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import './Customer.css'
 
 export default function Customer() {
+
+    // Mock Data - Temporary
+    const categories = ['Category 1', 'Category 2', 'Category 3'];
+    const menuItems = [
+        { name: 'Item 1', cost: '$10'},
+        { name: 'Item 2', cost: '$20'},
+        { name: 'Item 3', cost: '$30'},
+    ];
+
     return (
-        <h1>
-            Customer Page coming soon!
-        </h1>
+        <div className="customerPage">
+            <Link to="/" className="landingPageButton">Landing Page</Link>
+
+            <div className="customerContainer">
+                <div className="categories">
+                    <h2>Menu</h2>
+                    {categories.map((category, index) => (
+                        <p key={index}>{category}</p>
+                    ))}
+                </div>
+
+                <div className="items">
+                    <h2>Category X</h2>
+                    {menuItems.map((item, index) => (
+                        <div className="itemBox" key={index}>
+                            <p>{item.name}</p>
+                            <p>{item.cost}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
