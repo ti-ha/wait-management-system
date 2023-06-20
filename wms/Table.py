f430144addd1af6d08f249c3dc2dea65c900535c
Gordon Chau <gordonchau47@gmail.com>
2023-06-20 19:30:52 +1000

    Filled out some of the table class

--- wms/Table.py
+++ wms/Table.py
@@ -1,5 +1,6 @@
 import itertools
 import Order
+import Customer
 
 class Table:
 
@@ -9,13 +10,35 @@ class Table:
     def __init__(self):
         self.__id = next(Table.__id_iter)
         self.__orders = []
+        self.__customers = []
         self.__occupied = False
     
     def __init__(self, orders):
         self.__id = next(Table.__id_iter)
         self.__orders = orders
+        self.__customers = []
         self.__occupied = False
 
+    # Getters
+    def is_occupied(self):
+        return self.__occupied
+    
+    def get_orders(self):
+        return self.__orders
+    
+    def get_order(self, orderNumber):
+        return self.__orders[orderNumber]
 
+    def get_customers(self):
+        return self.__customers
 
+    # Adding to the list of customers/orders
+    def add_customers(self, customer):
+        self.__customers.append(customer)
 
+    def add_order(self, order):
+        self.__orders.append(order)
+
+    # Getting the current/final bill for the table
+    def request_bill(self):
+        return; 
