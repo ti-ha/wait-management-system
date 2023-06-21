44bd65b902e893f5fe2fe36726e371a153f154c6
Gordon Chau <gordonchau47@gmail.com>
2023-06-21 19:02:29 +1000

    Adjusting classes to fit with uml diagram

--- wms/Customer.py
+++ wms/Customer.py
@@ -1,11 +1,8 @@
 from User import User
-from Table import Table
 
 class Customer(User):
     def __init__(self, firstname="Guest", lastname="Customer", table=None):
         super().__init__(firstname, lastname)
-        if not isinstance(table, Table):
-            raise TypeError("Customer: constructor(): Object is not of type Table")
         self.__table = table
         if table is not None:
             self.__table.add_customers(self)
@@ -15,7 +12,5 @@ class Customer(User):
         return self.__table
     
     def set_table(self, table):
-        if not isinstance(table, Table):
-            raise TypeError("Customer: set_table(): Object is not of type Table")
         self.__table = table
         self.__table.add_customers(self)
--- wms/KitchenStaff.py
+++ wms/KitchenStaff.py
@@ -1,5 +1,4 @@
 from User import User
-from Order import Order
 
 class KitchenStaff(User):
     def __init__(self, firstname, lastname):
@@ -8,12 +7,8 @@ class KitchenStaff(User):
 
     # Add to list of orders
     def assign_order(self, order):
-        if not isinstance(order, Order):
-            raise TypeError("Kitchen Staff: assign_order(): Object is not of type Order")
         self.__orders.append(order)
 
     # Remove order once complete
     def remove_order(self, order):
-        if not isinstance(order, Order):
-            raise TypeError("Kitchen Staff: remove_order(): Object is not of type Order")
         self.__orders.remove(order)
