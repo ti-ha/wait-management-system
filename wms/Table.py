97f80a6baf54620a540dc317de8b525474d89704
tim <timhandley101@gmail.com>
2023-06-21 16:42:57 +1000

    Fixed some overloaded constructors that were not pythonic

--- wms/Table.py
+++ wms/Table.py
@@ -6,16 +6,14 @@ class Table:
     # Unique identifier starting from 0
     __id_iter = itertools.count()
 
-    def __init__(self):
-        self.__id = next(Table.__id_iter)
-        self.__orders = []
-        self.__occupied = False
-    
-    def __init__(self, orders):
+    def __init__(self, orders=None):
         self.__id = next(Table.__id_iter)
         self.__orders = orders
         self.__occupied = False
 
+        # Initialize empty list
+        if self.__orders == None:
+            self.__orders = []
 
 
 
