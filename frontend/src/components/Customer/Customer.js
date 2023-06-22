b4282a377c08cf3e00e534f2c0cf69002f7734d6
TheiSam <samialhilfi@gmail.com>
2023-06-22 16:48:44 +1000

    Updated customer view - clickable categories to display menu items. Aesthatic UI improvements

--- frontend/src/components/Customer/Customer.js
+++ frontend/src/components/Customer/Customer.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import './Customer.css'
 
+// Temporary mock data
+import { categories, menuItems } from '../../mocks/menu_items.js'
+
 export default function Customer() {
 
-    // Mock Data - Temporary
-    const categories = ['Category 1', 'Category 2', 'Category 3'];
-    const menuItems = [
-        { name: 'Item 1', cost: '$10'},
-        { name: 'Item 2', cost: '$20'},
-        { name: 'Item 3', cost: '$30'},
-    ];
+    const [currentCategory, setCurrentCategory] = useState("");
+    const [currentItems, setCurrentItems] = useState([]);
+
+    // First category is rendered by default
+    useEffect(() => {
+        setCurrentCategory(categories[0]);
+        setCurrentItems(menuItems.filter(item => item.category === categories[0]))
+    }, [])
+
+    const handleCategoryClick = (category) => {
+        setCurrentCategory(category);
+        setCurrentItems(menuItems.filter(item => item.category === category))
+    };
 
     return (
         <div className="customerPage">
@@ -20,18 +29,27 @@ export default function Customer() {
                 <div className="categories">
                     <h2>Menu</h2>
                     {categories.map((category, index) => (
-                        <p key={index}>{category}</p>
+                        <div 
+                            key={index} 
+                            className={`${category === currentCategory ? "selectedCategoryBox" : "categoryBox"}`}
+                            onClick={() => handleCategoryClick(category)}
+                        >
+                            <p>{category}</p>   
+                        </div>
                     ))}
                 </div>
 
                 <div className="items">
-                    <h2>Category X</h2>
-                    {menuItems.map((item, index) => (
-                        <div className="itemBox" key={index}>
-                            <p>{item.name}</p>
-                            <p>{item.cost}</p>
-                        </div>
-                    ))}
+                    <h2>{currentCategory}</h2>
+                    <div className="itemContainer">
+                        {currentItems.map((item, index) => (
+                            <div className="itemBox" key={index}>
+                                <img src={item.imageURL} alt={item.name}/>
+                                <p>{item.name}</p>
+                                <p>{item.cost}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
