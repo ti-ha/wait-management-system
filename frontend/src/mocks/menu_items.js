56f1a82e5c741f6fa7fc145c288ea42e5fc7ad66
TheiSam <samialhilfi@gmail.com>
2023-06-24 18:52:13 +1000

    Updated mock data

--- frontend/src/mocks/menu_items.js
+++ frontend/src/mocks/menu_items.js
@@ -1,16 +1,14 @@
-
-
 export const categories = ['Category 1', 'Category 2', 'Category 3'];
 
 export const menuItems = [
     { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 1', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 2', cost: '$20', category: 'Category 2', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
-    { name: 'Item 3', cost: '$30', category: 'Category 3', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 2', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 3', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 4', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 5', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 6', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 7', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 8', cost: '$10', category: 'Category 1', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 9', cost: '$20', category: 'Category 2', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
+    { name: 'Item 10', cost: '$30', category: 'Category 3', imageURL: 'https://t4.ftcdn.net/jpg/01/69/56/95/360_F_169569546_zaLG8x4tyIu3SDn1jYWXThVpMjCEbn8Q.jpg'},
 ];
