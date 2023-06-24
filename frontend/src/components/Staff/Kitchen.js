697438f0a674019a3162b638e222de598d9b3128
TheiSam <samialhilfi@gmail.com>
2023-06-24 22:26:59 +1000

    Created headers and buttons to link to other pages for both Kitchen and Wait Staff view

--- frontend/src/components/Staff/Kitchen.js
+++ frontend/src/components/Staff/Kitchen.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Kitchen.css'
+import { Button } from '@mui/material'
 
 export default function Kitchen() {
     return (
-        <h1>Kitchen View coming soon!</h1>
+        <div className="kitchenStaffContainer">
+            <header className="kitchenStaffHeader">
+                <h1>Kitchen View</h1>
+                <div className='headerButtons'>
+                    <Link to="/wait-staff">
+                        <Button variant="contained">
+                            Switch to Wait Staff View
+                        </Button>
+                    </Link>
+                    <Link to="/">
+                        <Button variant="contained">
+                            Landing Page
+                        </Button>
+                    </Link>
+                </div>
+            </header>
+        </div>
     )
 }
--- frontend/src/components/Staff/WaitStaff.js
+++ frontend/src/components/Staff/WaitStaff.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './WaitStaff.css'
+import { Button } from '@mui/material'
 
 export default function WaitStaff() {
     return (
-        <h1>Wait Staff View coming soon!</h1>
+        <div className="waitStaffContainer">
+            <header className="waitStaffHeader">
+                <h1>Wait Staff View</h1>
+                <div className='headerButtons'>
+                    <Link to="/kitchen">
+                        <Button variant="contained">
+                            Switch to Kitchen View
+                        </Button>
+                    </Link>
+                    <Link to="/">
+                        <Button variant="contained">
+                            Landing Page
+                        </Button>
+                    </Link>
+                </div>
+            </header>
+        </div>
     )
 }
