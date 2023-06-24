42fea72061467ed1f7ba077b2a6609efff77379a
Ravi Ramando <6368070+ramsnet@users.noreply.github.com>
2023-06-24 17:52:14 +1000

    draft 3

--- create_db.sql
+++ create_db.sql
@@ -1,16 +1,15 @@
 CREATE DATABASE flask_db;
 \c flask_db
-CREATE USER dummy WITH PASSWORD 'password';
-GRANT all privileges ON DATABASE flask_db TO dummy;
+CREATE USER admin WITH PASSWORD 'password';
+GRANT all privileges ON DATABASE flask_db TO admin;
 
-DROP TABLE IF EXISTS Menu
-CREATE TABLE menu(
-    categoryId      varchar(40), 
-    deals           varchar(40)
-);
+-- CREATE TABLE menu(
+--     categoryId      varchar(40), 
+--     deals           varchar(40),
+-- );
 
 CREATE TABLE category(
-    categoryId      serial primary key
+    categoryId      serial primary key,
     name            varchar(40) not null
 );
 
@@ -18,53 +17,52 @@ CREATE TABLE menu_items(
     itemId          serial primary key,
     name            varchar(40) not null,
     price           decimal(10, 2),
-    foreign key (categoryId) references Menu(categoryId)
+    foreign key (categoryId) references category(categoryId)
 );
 
-CREATE TABLE user_type as enum ('manager', 'customer', 'wait_staff', 'kitchen_staff');
+CREATE TYPE user_type AS ENUM ('manager', 'customer', 'wait_staff', 'kitchen_staff');
 
 CREATE TABLE users(
     userId          serial primary key,
     firstname       varchar(40),
-    lastname        varchar(40)
-    type            usertype              
+    lastname        varchar(40),
+    type            user_type              
 );
 
 CREATE TABLE customers(
     customerId      integer primary key,
-    foreign key (tableId) references table(tableId),
-    foreign key (customerId) references users(userId),
+    foreign key (tableId) references tables(tableId),
+    foreign key (customerId) references users(userId)
 );
 
 CREATE TABLE wait_staff(
-    staffId         integer primary key
+    staffId         integer primary key,
     foreign key (requestId) references requests(requestId),    
-    foreign key (staffId) references users(userId),
+    foreign key (staffId) references users(userId)
 );
 
 CREATE TABLE kitchen_staff(
-    staffId         integer primary key
+    staffId         integer primary key,
     foreign key (orderId) references orders(orderId),
-    foreign key (staffId) references users(userId),
+    foreign key (staffId) references users(userId)
 );
 
 CREATE TABLE orders(
     orderId         serial primary key,
     foreign key (itemId) references menu_items(itemId),
-    bill            boolean,
+    bill            boolean
 );
 
 CREATE TABLE requests(
     requestId       serial primary key,
-    foreign key (tableId) references table(tableId),
+    foreign key (tableId) references tables(tableId),
     summary         varchar(100) not null,
     complete        boolean
 );
 
 CREATE TABLE tables(
     tableId         serial primary key,
-    occupied        boolean,
-
+    occupied        boolean
 );
 
 
