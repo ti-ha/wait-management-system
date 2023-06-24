99ad5c6b894c20dd0744270ad87fc8d9f316df2d
TheiSam <samialhilfi@gmail.com>
2023-06-24 18:52:41 +1000

    Passing table number to the customer view page through useNavigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/staff" element={<StaffLanding />} />
         <Route path="/select-table" element={<TableSelection />} />
         <Route path="/manager" element={<Manager />} />
-        <Route path="/customer" element={<Customer />} />
+        <Route path="/customer/:tableNumber" element={<Customer />} />
       </Routes>
     </BrowserRouter>
   );
