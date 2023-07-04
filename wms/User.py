2870b9ac80d099b35ba8d73c25b7b5398bfbb2b6
Gordon Chau <gordonchau47@gmail.com>
2023-07-04 17:52:21 +1000

    Filled user docstring and function return types

--- wms/User.py
+++ wms/User.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import itertools
 
 class User:
@@ -5,16 +6,22 @@ class User:
     __id_iter = itertools.count()
 
     def __init__(self, firstname, lastname):
+        """ Constructor for the User Class
+
+        Args:
+            firstname (string): First Name of the User
+            lastname (string): Last Name of the User
+        """
         self.__id = next(User.__id_iter)
         self.__firstname = firstname
         self.__lastname = lastname
 
     # Getters
-    def get_firstname(self):
+    def get_firstname(self) -> str:
         return self.__firstname
 
-    def get_lastname(self):
+    def get_lastname(self) -> str:
         return self.__lastname
     
-    def get_id(self):
+    def get_id(self) -> itertools.count:
         return self.__id
