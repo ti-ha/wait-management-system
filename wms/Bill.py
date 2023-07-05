f795ede06c3a192410a7b60f03fae686cf723df0
Gordon Chau <gordonchau47@gmail.com>
2023-07-05 19:24:53 +1000

    Adding docstring for the bill class

--- wms/Bill.py
+++ wms/Bill.py
@@ -1,22 +1,32 @@
-import json
-
 class Bill:
-    # Constructor for the final bill
     def __init__(self, price):
+        """Constructor for the bill class
+
+        Args:
+            price (float): Starting price value of the bill
+        """
         self.__price = price
         self.__paid = False
 
     # Getters for price and paid status
-    def get_price(self):
+    def get_price(self) -> float:
         return self.__price
     
-    def is_paid(self):
+    def is_paid(self) -> bool:
         return self.__paid
 
-    # When bill is paid, set bool to true
     def pay(self):
+        """ When the bill is paid, set bool to true
+        """
         self.__paid = True
 
-    def jsonify(self) -> str:
+    def jsonify(self) -> dict:
+        """ Creates a dictionary containing the price and paid status of the 
+        bill
+
+        Returns:
+            dict: Dictionary containing the price and paid status of the 
+        bill
+        """
         output = {"price": self.__price, "paid": self.__paid}
         return output
