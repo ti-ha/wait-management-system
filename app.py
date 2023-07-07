2d3b4b62a1f52f1e3230b45ec7da5300e14bd4c9
Gordon Chau <gordonchau47@gmail.com>
2023-07-07 19:35:53 +1000

    Adding docstrings for app.py

--- app.py
+++ app.py
@@ -35,22 +35,28 @@ def call(msg, func, *args):
 
 @app.route('/')
 def home():
+    """ Home Page of the app """
     return jsonify({"message": "Hello world!"}), 200
 
 @app.route('/menu', methods=['GET'])
 def get_menu():
+    """ Gets the restaurant menu """
     return call(None, wms.menu_handler.jsonify)
 
 @app.route('/menu/categories', methods=['GET'])
 def get_categories():
+    """ Gets the menu categories """
     return call(None, wms.menu_handler.jsonify_categories)
 
 @app.route('/menu/categories', methods=['POST'])
 def create_category():
-    '''
+    """ Creates a new menu category
+
     JSON FORMAT:
-    {"name": "string"}
-    '''
+    {
+        "name": "string"
+    }
+    """
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -62,17 +68,20 @@ def create_category():
 
 @app.route('/menu/categories/<category>', methods=['GET'])
 def get_category(category):
+    """ Gets a specific menu category """
     return call(None, wms.menu_handler.jsonify_category, category)
 
 @app.route('/menu/categories/<category>', methods=['POST'])
 def add_menu_item_to_category(category):
-    '''
-    ADDING A NEW MENU ITEM TO CATEGORY.
+    """ Adds a new menu item to a specific category
+
     JSON FORMAT:
-    {"name": "string",
-        "price": float  ,
-        "image_url": "string"}
-    '''
+    {
+        "name": "string",
+        "price": float,
+        "image_url": "string"
+    }
+    """
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -88,37 +97,37 @@ def add_menu_item_to_category(category):
 
 @app.route('/menu/categories/<category>', methods=['DELETE'])
 def delete_category(category):
-    '''
-    REMOVING A SPECIFIC CATEGORY. DELETE METHOD FOR MENUITEM
-    IS IN THE SPECIFIC ROUTE FOR SPECIFIC MENU ITEM
-    '''
+    """ Removes a specific category """
     return call({"message": f"Successfully deleted category {category}"},
                 wms.menu_handler.remove_category, category)
     
 @app.route('/menu/categories/<category>/<menu_item>', methods=['GET'])
 def get_menu_item(category, menu_item):
+    """ Gets a specific menu item from a specific category """
     return call(None, wms.menu_handler.jsonify_menu_item, category, menu_item)
 
 @app.route('/menu/categories/<category>/<menu_item>', methods=['DELETE'])
 def delete_menu_item(category, menu_item):
+    """ Removes a specific menu item from a specific category """
     return call({"message": f"Successfully removed menuitem {menu_item} in category {category})"},
                 wms.menu_handler.remove_menu_item, category, menu_item)
 
 @app.route('/menu/deals', methods=['GET'])
 def get_deal():
+    """ Gets a menu deal """
     return call(None, wms.menu_handler.jsonify_deals)
 
 @app.route('/menu/deals', methods=['POST'])
 def create_deal():
-    '''
+    """ Creates a new menu deal
+
     JSON FORMAT:
-    {"discount": int,
-    "menu_items": [{"name": "string"},
-                    {"name": "string"}
-                    }]
+    {
+        "discount": int,
+        "menu_items": [{"name": "string"},{"name": "string"},...]
     }
     
-    '''
+    """
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -136,15 +145,19 @@ def create_deal():
 
 @app.route('/table', methods=['GET'])
 def get_table():
+    """ Gets all of the restaurant tables """
     return call(None, wms.table_handler.jsonify)
 
 @app.route('/table/add', methods=['POST'])
 def add_table():
-    '''
+    """ Creates a new table
+
     JSON FORMAT:
-    {"table_limit": int,
-     "orders": List<Order>}
-    '''
+    {
+        "table_limit": int,
+        "orders": List[Order]
+    }
+    """
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -162,16 +175,20 @@ def add_table():
 
 @app.route('/user', methods=['GET'])
 def get_user():
+    """ Gets all of the restaurant users """
     return call(None, wms.user_handler.jsonify)
 
 @app.route('/user/add', methods=['POST'])
 def add_user():
-    '''
+    """ Creates a new user
+
     JSON FORMAT:
-    {"first_name": string,
-     "last_name": string,
-     "user_type": string}
-    '''
+    {
+        "first_name": string,
+        "last_name": string,
+        "user_type": string
+    }
+    """ 
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -190,11 +207,14 @@ def add_user():
     
 @app.route('/table/add/customer', methods=['POST'])
 def add_table_customer():
-    '''
+    """ Adds a customer to a table
+
     JSON FORMAT:
-    {"table_id": int,
-     "customer_id": int}
-    '''
+    {
+        "table_id": int,
+        "customer_id": int
+    }
+    """ 
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -214,19 +234,24 @@ def add_table_customer():
     
 @app.route('/ordermanager', methods=['GET'])
 def get_order_manager():
+    """ Gets the order manager """
     return call(None, wms.om_handler.jsonify)
     
 @app.route('/ordermanager/orders', methods=['GET'])
 def get_orders():
+    """ Gets the list of orders present in the order manager """
     return call(None, wms.om_handler.jsonify_orders), 200
 
 @app.route('/ordermanager/orders/add/<table_id>' , methods=['POST'])
 def add_order(table_id):
-    '''
+    """ Adds an order to the order manager with the table it belongs to
+
     JSON FORMAT:
-    { "menu_items: [{"id": int}, ... , {"id": int}],
-      "deals:      [{"id": int}, ... , {"id": int}] }
-    '''
+    { 
+        "menu_items: [{"id": int}, ... , {"id": int}],
+        "deals:      [{"id": int}, ... , {"id": int}] 
+    }
+    """
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         obj = request.json
@@ -244,53 +269,63 @@ def add_order(table_id):
     
 @app.route('/ordermanager/orders/remove/<table_id>/<order_id>', methods=['DELETE'])
 def remove_order(table_id, order_id):
+    """ Removes an order from the order manager """
     return call({"message": "Successfully deleted order"},
                  wms.om_handler.remove_order, table_id, order_id)
     
 @app.route('/ordermanager/tables/<table_id>', methods=['GET'])
 def get_table_orders(table_id):
+    """ Gets all the orders of a specific table """
     return call(None, wms.om_handler.get_table_orders, table_id)
 
 
 @app.route('/ordermanager/tables/<table_id>/bill', methods=['GET'])
 def get_table_bill(table_id):
-    '''
+    """ Gets the bill for a table 
+
     EMPTY POST REQUEST. NO DATA EXPECTED
-    '''
+    """
     return call(None, wms.om_handler.calculate_and_return_bill, table_id)
 
 @app.route('/ordermanager/tables/<table_id>/bill', methods=['POST'])
 def pay_table_bill(table_id):
+    """ Endpoint to simulate bill payment for a table """
     return call({"message": "Successfully paid bill"},
                     wms.om_handler.pay_table_bill, table_id)
     
 @app.route("/ordermanager/orders/<order_id>", methods=['GET'])
 def get_order_by_id(order_id):
+    """ Gets an order by its ID value """
     return call(None, wms.om_handler.get_order_by_id, order_id)
 
 @app.route("/ordermanager/orders/<order_id>", methods=['DELETE'])
 def delete_order_by_id(order_id):
+    """ Deletes an order by its ID value """
     return call({"message": "Successfully removed order from ordermanager"},
                 wms.om_handler.delete_order_by_id, order_id)
     
 @app.route("/ordermanager/orders/<order_id>/state", methods=['GET'])
 def get_order_state(order_id):
+    """ Gets the current state of an order """
     return call(None, wms.om_handler.get_order_state, order_id)
 
 @app.route("/ordermanager/orders/<order_id>/state", methods=['POST'])
 def advance_order_state(order_id):
-    '''
+    """ Advances the state of a particular order
+
     EMPTY FOR NOW. WE WILL EXPAND THIS LATER TO INCLUDE STATE LEAPS IF WE WANT
-    '''
+    """
     return call({"message": f"Successfully changed state"},
                 wms.om_handler.change_order_state, order_id)
 
 @app.route("/ordermanager/orders/<order_id>/bill", methods=['GET'])
 def get_order_bill(order_id):
+    """ Gets the bill for an order """
     return call(None, wms.om_handler.get_order_bill, order_id)
 
 @app.route("/ordermanager/orders/<order_id>/bill", methods=['POST'])
 def pay_order_bill(order_id):
+    """ Endpoint to simulate bill payment for an order """
     return call({"message": "Successfully paid bill"},
                 wms.om_handler.pay_order_bill, order_id)
 
