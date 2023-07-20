33875d22efdb30afdff8f7d0673d8e7ee8215f10
tim <timhandley101@gmail.com>
2023-07-20 23:25:41 +1000

    Fixed a bug

--- wms/Application.py
+++ wms/Application.py
@@ -14,25 +14,25 @@ class Application():
         self.__restaurant_manager_handler = RestaurantManagerHandler(RestaurantManager(), self.menu_handler)
 
     @property
-    def menu_handler(self):
+    def menu_handler(self) -> MenuHandler:
         return self.__menu_handler
     
     @property
-    def table_handler(self):
+    def table_handler(self) -> TableHandler:
         return self.__table_handler
     
     @property
-    def om_handler(self):
+    def om_handler(self) -> OrderManagerHandler:
         return self.__om_handler
     
     @property
-    def user_handler(self):
+    def user_handler(self) -> UserHandler:
         return self.__user_handler
     
     @property
-    def srm_handler(self):
+    def srm_handler(self) -> SRMHandler:
         return self.__srm_handler
     
     @property
-    def restaurant_manager_handler(self):
+    def restaurant_manager_handler(self) -> RestaurantManagerHandler:
         return self.__restaurant_manager_handler
