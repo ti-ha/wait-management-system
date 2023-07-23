e34399f9b3488f45ac8faf3a5422dd013f44d409
Gordon Chau <gordonchau47@gmail.com>
2023-07-23 15:22:43 +1000

    Fixing order status sorting

--- wms/RestaurantManagerHandler.py
+++ wms/RestaurantManagerHandler.py
@@ -38,7 +38,7 @@ class RestaurantManagerHandler():
             sorted by their table size
         """
         sorted_list = sorted(self.table_handler.tables, key=lambda table: table.table_limit)
-        return self.jsonify_tables(sorted_list)
+        return self.jsonify_tables_size(sorted_list)
     
     def tables_sort_orders(self) -> dict:
         """ Sorts the tables by their order status
@@ -48,7 +48,7 @@ class RestaurantManagerHandler():
             sorted by their order status
         """
         sorted_list = sorted(self.table_handler.tables, key=cmp_to_key(self.table_order_compare))
-        return self.jsonify_tables(sorted_list)
+        return self.jsonify_tables_orders(sorted_list)
 
     def table_order_compare(self, table_1: Table, table_2: Table) -> int:
         """ Compare function to compare two tables by their order status
@@ -61,14 +61,51 @@ class RestaurantManagerHandler():
             int: Integer representing the difference in order status between 
             two tables
         """
-        return sum([i.state_value for i in table_1.orders]) - sum([i.state_value for i in table_2.orders])
+        table_1_orders = self.get_min_order_state(table_1)[0]
+        table_2_orders = self.get_min_order_state(table_2)[0]
+        return table_1_orders - table_2_orders
     
-    def jsonify_tables(self, table_list : list[Table]) -> dict:
+    def get_min_order_state(self, table: Table) -> list:
+        """ Get the lowest order status of the table with its corresponding 
+        order id
+
+        Args:
+            table (Table): Table that contains the list of orders to go through
+
+        Returns:
+            tuple: Contains the lowest order status and its corresponding order
+            id
+        """
+        order_states = [[i.state_value, i.state, i.id] for i in table.orders if i.state_value != -1]
+        sorted_states = sorted(order_states, key=lambda value: value[0])
+        return sorted_states[0]
+    
+    def jsonify_tables_size(self, table_list : list[Table]) -> dict:
         """ Creates a dictionary containing the id, availability string, table
-        limit and occupied boolean of the table  
+        limit and occupied boolean of the table that have been sorted by table 
+        limit 
 
         Returns:
             Dict: A dictionary containing the id, availability string, table
-        limit and occupied boolean of the table  
+        limit and occupied boolean of the table that have been sorted by table
+        limit
         """
         return {"tables": [table.jsonify() for table in table_list]}
+    
+    def jsonify_tables_orders(self, table_list : list[Table]) -> dict:
+        """ Creates a dictionary containing the id, availability string, table
+        limit, occupied boolean and the state and order_id of lowest order
+        status of the table (ignoring deleted orders)
+
+        Returns:
+            Dict: A dictionary containing the id, availability string, table
+        limit, occupied boolean and the state and order_id of lowest order
+        status of the table (ignoring deleted orders)
+        """
+        table_info = []
+        for table in table_list:
+            table_dict = table.jsonify()
+            table_dict["state"] = self.get_min_order_state(table)[1]
+            table_dict["order_id"] = self.get_min_order_state(table)[2]
+            table_info.append(table_dict)
+        return {"tables": table_info}
