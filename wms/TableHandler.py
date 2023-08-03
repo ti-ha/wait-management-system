c7f4133684f5f04a13ead686832f368dda4e0f83
Gordon Chau <gordonchau47@gmail.com>
2023-08-03 17:29:21 +1000

    Fixing docstring/comment + add order import

--- wms/TableHandler.py
+++ wms/TableHandler.py
@@ -1,8 +1,9 @@
 from .Table import Table
 from .Customer import Customer
+from .Order import Order
 
 class TableHandler():
-    def __init__(self) -> None:
+    def __init__(self):
         """ Constructor for the TableHandler Class """
         self.__tables = []
 
@@ -11,21 +12,21 @@ class TableHandler():
         """ Returns the list of tables """
         return self.__tables
     
-    def add_table(self, table_limit, orders):
+    def add_table(self, table_limit: int, orders: list[Order]):
         """ Adds a table to the restaurant
 
         Args:
-            table_limit (Integer): Maximum number of customers at the table
-            orders (List[Order]): Orders to be predefined with the table 
+            table_limit (int): Maximum number of customers at the table
+            orders (list[Order]): Orders to be predefined with the table 
         """
         table = Table(table_limit, orders)
         self.__tables.append(table)
     
-    def add_customer(self, table_id, customer: Customer) -> bool:
+    def add_customer(self, table_id: int, customer: Customer) -> bool:
         """ Adds a customer to the table
 
         Args:
-            table_id (Integer): ID of the table that customer is being added to
+            table_id (int): ID of the table that customer is being added to
             customer (Customer): Customer to be added to the table
         """
         table = self.id_to_table(table_id)
@@ -36,16 +37,16 @@ class TableHandler():
         limit and occupied boolean of the table  
 
         Returns:
-            Dict: A dictionary containing the id, availability string, table
+            dict: A dictionary containing the id, availability string, table
         limit and occupied boolean of the table  
         """
         return {"tables": [table.jsonify() for table in self.tables]}
 
-    def id_to_table(self, id) -> Table:
+    def id_to_table(self, id: int) -> Table:
         """ Converts a given id to a table object
 
         Args:
-            id (Integer): ID value of the table
+            id (int): ID value of the table
 
         Returns:
             Table: Table object that matches the provided ID value
